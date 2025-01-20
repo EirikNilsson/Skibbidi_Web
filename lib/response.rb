97edd6fbc7097619faf754a919d39026e578f91c
EirikNilsson <eirik.haugennilsson@elev.ga.ntig.se>
2025-01-20 18:56:21 +0100

    erb and post

--- lib/response.rb
+++ lib/response.rb
@@ -12,6 +12,7 @@ class Response
         'Content-Type' => content_type,
         'Content-Length' => body.bytesize
       }
+      
   
       session.print "HTTP/1.1 #{status_code} #{status_message}\r\n"
       headers.each { |key, value| session.print "#{key}: #{value}\r\n" }
--- lib/tcp_server.rb
+++ lib/tcp_server.rb
@@ -1,39 +1,57 @@
 require 'socket'
+require 'erb'
 require_relative 'request'
 require_relative 'router'
 require_relative 'response'
 
+
 class HTTPServer
+
+  def erb(template_path)
+    file_path = "#{template_path}.erb"       
+    template = File.read(file_path)          
+    ERB.new(template).result(binding)        
+  end
+
+
   def initialize(port)
     @port = port
     @router = Router.new
+    @fruits = ['Mango', 'Banan']
 
-    @router.add_route('GET', '/fruit') do
-      "<html><body><h1>Welcome to my Fruit Page!</h1></body></html>"
+    
+
+    @router.add_route('GET', '/') do
+      erb("views/index")  
     end
     @router.add_route('GET', '/fruit/:id') do |params|
-      fruits = ['Apple', 'Banana', 'Orange', 'Pear', 'Grape']
       id = params['id'].to_i
-      fruit = fruits[id] || 'Unknown'
+      @fruit = fruits[id] || 'Unknown'
       "<html><body><h1>Fruit: #{fruit}</h1></body></html>"
     end
-    @router.add_route('POST', '/fruit/new') do |params|
-      new_fruit = params['name'] || 'Unnamed Fruit'
+
+    @router.add_route('POST', '/') do |params|
+      @new_fruit = params['name'] 
+      @fruits << @new_fruit
       <<-HTML
       <!DOCTYPE html>
       <html lang="en">
       <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>New Fruit Created</title>
+          <title>Fruit Added</title>
       </head>
       <body>
-          <h1>New Fruit Created: #{new_fruit}</h1>
-          <a href="/fruit">Go back</a>
+          <h1>New Fruit Added: #{@new_fruit}</h1>
+          <a href="/">Go Back to Fruit Page</a>
       </body>
       </html>
       HTML
     end
+    
+
+
+
     
     
     
@@ -58,7 +76,7 @@ class HTTPServer
       route_block = @router.match_route(request)
 
       if route_block
-        html = route_block.call(request.params) # Skicka parametrarna till blocket
+        html = route_block.call(request.params) 
         Response.build(session, status_code: 200, body: html)
       else
         html = "<html><body><h1>404 Not Found</h1></body></html>"
